2ca78aaa451d06adb9ac3eaf1be59971f64e6fc0
ducdmn <ndminhduc@gmail.com>
2020-01-07 16:40:49 +0700

    Add example project

---
+++ SignalRHubClient/Program.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRHubClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var connection = new HubConnection("http://127.0.0.1:8088/");
+            var connection = new HubConnection("http://localhost:8045/");
+            var myHub = connection.CreateHubProxy("TaskHub");
+
+            //Console.WriteLine("Enter your name");
+            //string name = Console.ReadLine();
+
+            connection.Start().ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error opening the connection:{0}", task.Exception.GetBaseException());
+                }
+                else
+                {
+                    Console.WriteLine("Connected");
+
+                    myHub.On<string, string>("addMessage", (s1, s2) =>
+                    {
+                        Console.WriteLine(s1 + ": " + s2);
+                    });
+
+                    //while (true)
+                    //{
+                    //    string message = Console.ReadLine();
+
+                    //    if (string.IsNullOrEmpty(message))
+                    //    {
+                    //        break;
+                    //    }
+
+                    //    myHub.Invoke<string>("Send", name, message).ContinueWith(task1 =>
+                    //    {
+                    //        if (task1.IsFaulted)
+                    //        {
+                    //            Console.WriteLine("There was an error calling send: {0}", task1.Exception.GetBaseException());
+                    //        }
+                    //        else
+                    //        {
+                    //            Console.WriteLine(task1.Result);
+                    //        }
+                    //    });
+                    //}
+                }
+            }).Wait();
+
+            myHub.Invoke<string>("SendTask1",  "I'm doing something!!!").ContinueWith(task1 =>
+                {
+                    if (task1.IsFaulted)
+                    {
+                        Console.WriteLine("There was an error calling send: {0}", task1.Exception.GetBaseException());
+                    }
+                    else
+                    {
+                        Console.WriteLine(task1.Result);
+                    }
+                }).Wait();
+
+            Console.Read();
+            connection.Stop();
+        }
+    }
+}
---
+++ SignalRHubServer/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.Owin;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(SignalRHubServer.Program.Startup))]
+namespace SignalRHubServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string url = "http://127.0.0.1:8088";
+            string url = "http://localhost:8088";
+
+            var app = WebApp.Start(url);
+            Console.ReadKey();
+        }
+
+        public class Startup
+        {
+            public void Configuration(IAppBuilder app)
+            {
+                //app.UseCors(CorsOptions.AllowAll);
+                app.MapSignalR();
+            }
+        }
+
+        [HubName("TaskHub")]
+        public class MyHub: Hub {
+            public void Send(string name, string message)
+            {
+                Console.WriteLine(name + ": " + message);
+                Clients.All.addMessage(name, message);
+            }
+
+            public void SendTask(dynamic request)
+            {
+                Console.WriteLine(request.ToString());
+            }
+        }
+    }
+}
